70656812530a1aae4b7b7e2b32d425c2a0508e1e
Sam Lazarus <lazarus.sam@gmail.com>
2020-04-04 18:38:43 -0400

    improves awk usage(?)

--- j.sh
+++ j.sh
@@ -38,36 +38,35 @@ do
 	case "$1" in
 		-r) rating="$2"; shift;;
 		-f) from="$2"; shift;;
-		-t) to="$2"; echo "to '$2'"; shift;;
+		-t) to="$2"; shift;;
 		--) shift; break;;
 	esac
 	shift
 done
 
-# if we're viewing, just retrieve
+# if we're viewing, clip the file and return entries in the range
 if [ ! -z $from ]; then
+	# we have a starting date
 	fromDate=$(date --date="$from" -I)
 	if [ ! -z $to ]; then
+		# we have an ending date
 		toDate=$(date --date="$to" -I)
-		awk 'BEGIN { foundFrom=0; fromDate="'"$fromDate"'"; toDate="'"$toDate"'"; }
-			{
-				if ($1 == "===") {
-					if ($2 > toDate)						{ exit; }
-					if (foundFrom == 0 && $2 >= fromDate)	{ foundFrom++; }
-				}
-				if (foundFrom > 0) { print $0; }
-			}' <$journal
+		awk 'BEGIN { found=0; from="'"$fromDate"'"; to="'"$toDate"'"; }
+			/^=== / && ($2 > to) { exit }
+			/^=== / && (found == 0) && ($2 >= from) { found++ }
+			(found > 0) { print }' <$journal
 	else
-		awk 'BEGIN { foundFrom=0; fromDate="'"$fromDate"'"; }
-			{
-				if (foundFrom==0 && $1 == "===" && $2 >= fromDate) { foundFrom++; }
-				if (foundFrom>0) { print $0; }
-			}' <$journal
+		# we don't have an ending date
+		awk 'BEGIN { found=0; from="'"$fromDate"'"; }
+			/^=== / && (found == 0) && ($2 >= from) { found++ }
+			(found > 0) { print }' <$journal
 	fi
 	exit
 elif [ ! -z $to ]; then
+	# we only have an ending date
 	toDate=$(date --date="$to" -I)
-	awk 'BEGIN { toDate="'"$toDate"'"; } { if ($1 == "===" && $2 > toDate) { exit; } print $0; }' <$journal
+	# print until we find a date equal to or after the end date
+	awk 'BEGIN { to="'"$toDate"'"; } /^=== / && ($2 > to) { exit } { print }' <$journal
 	exit
 fi
 
